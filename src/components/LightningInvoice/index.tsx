6cc2c9d477f63c1fed6ab2dd70131a6beb4e4a08
evildido <fraga_jonathan@protonmail.ch>
2023-01-22 17:39:16 +0100

    add WebLN

--- src/components/LightningInvoice/index.tsx
+++ src/components/LightningInvoice/index.tsx
@@ -15,11 +15,14 @@ export const LightningInvoice = ({
 }: LightningInvoiceProps) => {
   return (
     <div className={content}>
-      <p className={text}>scan this qr code & pay the lightning invoice:</p>
+      <p className={text}>scan (or click) this qr code & pay the lightning invoice:</p>
       {isLoading ? (
         <div>loading...</div>
       ) : invoiceData ? (
+        <a href={`lightning:` + invoiceData.payment_request}>
         <QRCodeSVG value={invoiceData.payment_request} />
+        </a>
+        
       ) : (
         <div>could not generate invoice, please retry later</div>
       )}
