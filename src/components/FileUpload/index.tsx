51d32428cb96a71dbad6384319eb50832f12ba75
evildido <fraga_jonathan@protonmail.ch>
2023-01-28 17:38:36 +0100

    Fix image resize

--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -3,11 +3,28 @@ import { UploadedFile } from "../../App";
 import { Button, Input } from "../../atoms";
 import { uploadFile } from "../../clients";
 import { content } from "../../common.css";
+import { fromBlob } from "image-resize-compress";
 
 interface FileUploadProps {
   availableHashes: string[];
   onFileUpload: (spentHashes: string[], uploadedFile: UploadedFile) => void;
-}
+};
+
+const resizeFile = (file: File, type: string) => {
+  const fileResized = fromBlob(file, 60, 2000, 2000, type)
+  .then((blob) => {
+    console.log('blob', blob)
+    return new File([blob], file.name, {type: file.type});
+  });
+  return fileResized;
+};
+
+// check image size. Return true if size is more than 3Mo
+const checkSize = (file: File) => {
+  console.log("file size", file.size / 1024 / 1024);
+  const size = file.size / 1024 / 1024;
+  return size > 3;
+};
 
 export const FileUpload = ({
   availableHashes,
@@ -17,7 +34,13 @@ export const FileUpload = ({
 
   const uploadFiles = async () => {
     if (!files?.[0]) return;
-    const data = await uploadFile(files[0], availableHashes[0]);
+    // resize image if size is more than 7Mo
+    //const file: File = checkSize(files[0]) ? await resizeFile(files[0], files[0].type.split("/")[1]) : files[0];
+    let file: File = files[0];
+    if (checkSize(files[0])) {
+      file = await resizeFile(files[0], files[0].type.split("/")[1]);
+    }
+    const data = await uploadFile(file, availableHashes[0]);
     if (data?.success)
       onFileUpload([availableHashes[0]], {
         name: files[0].name,
