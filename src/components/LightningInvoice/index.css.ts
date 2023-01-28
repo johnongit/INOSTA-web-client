8c7fe0e54e972400b786a272e362ad0dd9689704
evildido <fraga_jonathan@protonmail.ch>
2023-01-28 23:08:28 +0100

    enchance interface

--- src/components/LightningInvoice/index.css.ts
+++ src/components/LightningInvoice/index.css.ts
@@ -1,11 +1,28 @@
 import { style } from "@vanilla-extract/css";
-import { centeredRow } from "../../common.css";
+import { centeredRow, centered } from "../../common.css";
+
 
 export const invoiceDisplay = style([
   centeredRow,
   {
-    width: "100%",
+    width: "max-content",
+    maxWidth: "100%",
     flexWrap: "wrap",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    
   },
 ]);
-export const invoiceDisplayQRCode = style({ margin: "0 2rem" });
+
+export const content = style({
+  width: "max-content",
+  maxWidth: "100%",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
+
+export const /* A style that is applied to the `QRCode` component. */
+invoiceDisplayQRCode = style({ margin: "1rem 2rem" });
